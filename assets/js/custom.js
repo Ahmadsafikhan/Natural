cda02b6d8499ea1a00e2ac3022927bfd7344c8d1
Ahmadsafikhan <safionair@gmail.com>
2021-08-31 11:55:49 +0500

    klkl

---
+++ assets/js/custom.js
@@ -0,0 +1,37 @@
+// carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// sticky navbar
+window.onscroll=function(){
+    myfunction()
+};
+var navbar = document.getElementById("mynavbar");
+function myfunction(){
+    if(window.pageYOffset>=100){
+        navbar.classList.add("sticky")
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
+
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+});
